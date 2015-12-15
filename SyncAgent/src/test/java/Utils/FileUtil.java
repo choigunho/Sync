5dc2f598cb27c3e4a2d992fe050511654094587b
gunho14.choi <gunho14.choi@samsung.com>
2015-12-15 16:57:41 +0900

    log add

--- SyncAgent/src/test/java/Utils/FileUtil.java
+++ SyncAgent/src/test/java/Utils/FileUtil.java
@@ -39,7 +39,7 @@ public class FileUtil {
 		
 		try {
 			await().until(checkCountCallable(respectedCount, userId, targetDir));
-			System.out.println(respectedCount + "개 파일(폴더) PC 동기화 완료");
+			System.out.println(respectedCount + "개 파일(폴더) PC 동기화 완료 " + getFileList(targetDir, userId));
 //			System.out.println(targetDir.getName());
 //			System.out.println(getFileList("/moveWEB", userId));
 		}catch(Exception e){
